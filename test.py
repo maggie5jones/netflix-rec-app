88bb8bbf3407157b0f3e70509df6efaa6169bc46
maggie5jones <90584696+maggie5jones@users.noreply.github.com>
2022-05-03 15:23:59 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import movieposters as mp
 
 link = mp.get_poster(title='breakfast club')
+# link = mp.get_poster(id='tt0088847')  # can also be found using movie's id
+# link = mp.get_poster(id=88847)
+
 print(link)
