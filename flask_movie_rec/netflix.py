ef7311e918e3e2ae488e479803b5f207db43df2d
maggie5jones <90584696+maggie5jones@users.noreply.github.com>
2022-05-03 09:46:16 -0700

    Update netflix.py

--- flask_movie_rec/netflix.py
+++ flask_movie_rec/netflix.py
@@ -1,2 +1,86 @@
-import pandas as pd
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
+
+netflix_overall=pd.read_csv("netflix_titles.csv")
+
+#removing stopwords
+tfidf = TfidfVectorizer(stop_words='english')
+
+#Replace NaN with an empty string
+netflix_overall['description'] = netflix_overall['description'].fillna('')
+
+#Construct the required TF-IDF matrix by fitting and transforming the data
+tfidf_matrix = tfidf.fit_transform(netflix_overall['description'])
+
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+indices = pd.Series(netflix_overall.index, index=netflix_overall['title']).drop_duplicates()
+
+def get_recommendations(title, cosine_sim=cosine_sim):
+    idx = indices[title]
+
+    # Get the pairwsie similarity scores of all movies with that movie
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    # Sort the movies based on the similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the scores of the 10 most similar movies
+    sim_scores = sim_scores[1:11]
+
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
+
+    # Return the top 10 most similar movies
+    return netflix_overall['title'].iloc[movie_indices]
+  
+# new recommedation system??
+filledna=netflix_overall.fillna('')
+
+def clean_data(x):
+    return str.lower(x.replace(" ", ""))
+
+features=['title','director','cast','listed_in','description']
+filledna=filledna[features]
+
+for feature in features:
+    filledna[feature] = filledna[feature].apply(clean_data)
+    
+def create_soup(x):
+    return x['title']+ ' ' + x['director'] + ' ' + x['cast'] + ' ' +x['listed_in']+' '+ x['description']  
+
+filledna['soup'] = filledna.apply(create_soup, axis=1)
+
+count = CountVectorizer(stop_words='english')
+count_matrix = count.fit_transform(filledna['soup'])
+
+cosine_sim2 = cosine_similarity(count_matrix, count_matrix)
+
+filledna=filledna.reset_index()
+indices = pd.Series(filledna.index, index=filledna['title'])
+
+def get_recommendations_new(title, cosine_sim=cosine_sim):
+    title=title.replace(' ','').lower()
+    idx = indices[title]
+
+    # Get the pairwsie similarity scores of all movies with that movie
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    # Sort the movies based on the similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the scores of the 10 most similar movies
+    sim_scores = sim_scores[1:11]
+
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
+
+    # Return the top 10 most similar movies
+    return netflix_overall['title'].iloc[movie_indices]
+
 
