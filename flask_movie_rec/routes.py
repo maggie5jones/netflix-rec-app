cb4cddf2efb80247cb7ce865cd8bb87aae97e5c8
Maggie Jones <mkjones@hmc.edu>
2022-05-03 10:31:43 -0700

    up

--- flask_movie_rec/routes.py
+++ flask_movie_rec/routes.py
@@ -1,16 +1,22 @@
-from flask import Blueprint, render_template
+import requests
+from flask import Blueprint, render_template, current_app, request
+from ./netflix.py import get_recommendations_new
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
-    #TODO: write things here to incorporate backend code
-    results = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    poster_url = 'http://api.themoviedb.ord/3/movie/{imdbid}/images?api_key=1a3b037b3193bfd1535049e30f4d4890'
 
-    #TODO: loop through items from results
     movies = []
-    for result in results:
-        movie_data = 10
-        movies.append(movie_data)
+
+    if request.method == 'POST':
+        #TODO: write things here to incorporate backend code
+        results = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        #TODO: loop through items from results
+        for result in results:
+            movie_data = 10
+            movies.append(movie_data)
 
     return render_template('index.html', movies=movies)
