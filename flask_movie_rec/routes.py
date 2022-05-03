56aad0f8af610a1cf30a63b868a34dd48780c5ed
Maggie Jones <mkjones@hmc.edu>
2022-05-03 09:36:37 -0700

    app working

--- flask_movie_rec/routes.py
+++ flask_movie_rec/routes.py
@@ -4,4 +4,13 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    #TODO: write things here to incorporate backend code
+    results = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    #TODO: loop through items from results
+    movies = []
+    for result in results:
+        movie_data = 10
+        movies.append(movie_data)
+
+    return render_template('index.html', movies=movies)
