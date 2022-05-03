acfc772b42368fbe9ef46bb431d6ee65968ba7fb
maggie5jones <90584696+maggie5jones@users.noreply.github.com>
2022-05-03 10:34:30 -0700

    Update netflix.py

--- flask_movie_rec/netflix.py
+++ flask_movie_rec/netflix.py
@@ -3,6 +3,8 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)import sea
 import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.pyplot as plt
+import requests
+import urllib
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
 
@@ -82,3 +84,24 @@ def get_recommendations_new(title, cosine_sim=cosine_sim):
 
     # Return the top 10 most similar movies
     return netflix_overall['title'].iloc[movie_indices]
+
+def imdb_id_from_title(title):
+    """ return IMDB id for search string
+
+        Args::
+            title (str): the movie title search string
+
+        Returns: 
+            str. IMDB id, e.g., 'tt0095016' 
+            None. If no match was found
+
+    """
+    pattern = 'http://www.imdb.com/xml/find?json=1&nr=1&tt=on&q={movie_title}'
+    url = pattern.format(movie_title=urllib.quote(title))
+    r = requests.get(url)
+    res = r.json()
+    # sections in descending order or preference
+    for section in ['popular','exact','substring']:
+        key = 'title_' + section 
+        if key in res:
+            return res[key][0]['id']
